361c94206dc8a320d7f4550c8aa2dac4acc94a02
jsurt <jacksurtees@gmail.com>
2019-02-11 21:05:28 -0500

    fixed database issue

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/task-timer";
+  process.env.DATABASE_URL || "mongodb://localhost/task-timer-db";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-task-timer";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-task-timer-db";
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = "VERYSECRETKEY";
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
